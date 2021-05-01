e4a09e566b4935ed7d2e470c5a59bc349c5d97e6
jungkh405@naver.com <jungkh405@naver.com>
2021-05-01 14:12:41 +0900

    chore : 프로젝트 생성

---
+++ src/main/java/com/cacaofriendsshop/CacaoFriendsShopApplication.java
@@ -0,0 +1,13 @@
+package com.cacaofriendsshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacaoFriendsShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacaoFriendsShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cacaofriendsshop/CacaoFriendsShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cacaofriendsshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacaoFriendsShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
